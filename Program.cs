88fa120bb0cb7993c6873ab7a4a22710ef86b972
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 18:38:31 -0600

    Logica Aplicada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿// Escriba un programa que muestre los números del 1 al 10 junto con su cuadrado.
+
+int num = 1;
+
+do
+{
+    Console.WriteLine(num + " - " + (num * num));
+    num++;
+} while (num <= 10);
